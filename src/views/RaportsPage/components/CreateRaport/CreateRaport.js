0acc265b2806a6fd0bf8b40289e4c7fe0bd9cb17
nejtos <nejcik@interia.pl>
2022-05-29 03:07:57 +0200

    working adding reports

--- src/views/RaportsPage/components/CreateRaport/CreateRaport.js
+++ src/views/RaportsPage/components/CreateRaport/CreateRaport.js
@@ -1,39 +1,71 @@
 import "./CreateRaport.css"
-// import axios from "axios";
-// import { baseApiUrl } from "../../../../services/routes";
-// import { useState } from "react";
+import axios from "axios";
+import { baseApiUrl } from "../../../../services/routes";
+import { useState } from "react";
 
 const CreateRaport = () => {
 
-    // const [id_elementu, setIDElementu] = useState("")
-    // const [nazwa_elementu, setNazwaElementu] = useState("")
-    
-    // const handleChange = () => {
-    //     axios.post(`${baseApiUrl}/storage/create`, {
-    //         id_elementu: id_elementu,
-    //         nazwa_elementu: nazwa_elementu,
-    //         })
-    //         .then(() => {
-    //             console.log("Dodano nowy element");
-    //         })
-    //         .catch((error) => {
-    //             console.log(error.response.data);
-    //         });
-    // }
+    const [id_raportu, setIDRaportu] = useState("")
+    const [inputList, setInputList] = useState([{ id_produktu: "", opis: "" }]);
+
+    const handleChange = () => {
+        axios.post(`${baseApiUrl}/raportsrows/create`, {
+            id_raportu: id_raportu,
+            inputList: inputList,
+            })
+            .then(() => {
+                console.log("Dodano nowy raport");
+            })
+            .catch((error) => {
+                console.log(error.response.data);
+            });
+    }
+
+    const handleInputChange = (e, index) => {
+        const { name, value } = e.target;
+        const list = [...inputList];
+        list[index][name] = value;
+        setInputList(list);
+    };
+
+    // handle click event of the Remove button
+    const handleRemoveClick = index => {
+        const list = [...inputList];
+        list.splice(index, 1);
+        setInputList(list);
+    };
+
+    // handle click event of the Add button
+    const handleAddClick = () => {
+        setInputList([...inputList, { id_produktu: "", opis: "" }]);
+    };
 
     const AcceptChanges = () => {
+        handleChange();
         window.location.reload(false);
     }
 
     return (
         <div>
             <div className="CreateRaportPanelWrapper">
-                <label className="CreateRaportLabel" > ID raportu </label> 
-                    <input className="CreateRaportInput"/> 
-                <label className="CreateRaportLabel" > ID produktu </label>
-                    <input className="CreateRaportInput"/>
-                <label className="DetailsLabel" > Opis </label>
-                    <input className="DetailsInput"/>
+                <label className="CreateRaportLabel" > ID raportu </label>
+                    <input className="CreateRaportInput" onChange={e => { setIDRaportu(e.target.value) }} />
+                {inputList.map((x, i) => {
+                    return (
+                        <div className="Box" key={i}>
+                            <div className="FirstRow">
+                            <label className="CreateRaportLabel" > ID produktu </label>
+                                <input className="CreateRaportInput" name="id_produktu" onChange={e => { handleInputChange(e, i) }} />
+                            </div>
+                            <div className="SecondRow" >
+                            <label className="DetailsLabel" > Opis </label>
+                                <input className="DetailsInput" name="opis" onChange={e => { handleInputChange(e, i) }} />
+                            </div>
+                            {inputList.length !== 1 && <button className="removeButton" onClick={() => handleRemoveClick(i)}> Usuń </button>}
+                            {inputList.length - 1 === i && <button className="addButton" onClick={handleAddClick}> Dodaj </button>}
+                        </div>
+                    );
+                })}
             </div>
             <div className="CreateRaportAcceptButton">
                 <button className="CreateRaportAcceptExteriorButtonBox">
@@ -42,8 +74,6 @@ const CreateRaport = () => {
                     </div>
                 </button>
             </div>
-            {/* <input className="FirstBox" name="id_elementu" onChange={(e) => { setIDElementu(e.target.value) }} />
-            <input className="SecondBox" name="nazwa_elementu" onChange={(e) => { setNazwaElementu(e.target.value) }} /> */}
         </div>
     );
 }
